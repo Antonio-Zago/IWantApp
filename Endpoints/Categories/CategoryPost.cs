d39477d279166ef5379cf0ce89ee25c7e5d237fe
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2022-08-03 08:47:54 -0300

    Criação dos domain categoria e produtos e dos endpoints de categoria

---
+++ Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,20 @@
+﻿using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories
+{
+    public class CategoryPost
+    {
+        //Rota
+        public static string Template => "/categories";
+
+        public static string[] Methods => new string[] { HttpMethod.Post.ToString()};
+
+        public static Delegate handle => Action;
+
+
+        public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext applicationDbContext) 
+        {
+            return Results.Ok("Ok");
+        }
+    }
+}
---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Infra.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categoryes { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Description).HasMaxLength(500);
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name).IsRequired();
+
+
+            modelBuilder.Entity<Category>()
+                .Property(c => c.Name).IsRequired();
+
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder modelConfigurationBuilder) 
+        {
+            modelConfigurationBuilder.Properties<string>()
+                .HaveMaxLength(100);
+        }
+    }
+}
